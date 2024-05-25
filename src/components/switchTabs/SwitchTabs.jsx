41775db37cde1a34f1301015bfa3967c515127b8
Tan <tan10tvo@gmail.com>
2024-05-25 14:28:28 -0600

    state hook in switchtabs

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-
+import React, {useState} from "react";
+import "./style.scss";
 const SwitchTabs = ({data, onTabChange}) =>{
+    const [selectedTab, setSelectedTab ] = useState(0)
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, index) => {
+        setLeft()
+    } 
+
     return (
-        <div className="switchingTabs">SwitchTabs</div>
+        <div className="switchingTabs">
+            <div className="tabItems">
+                {data.map((tab, index) => (
+                    <span key={index} className={`tabItem`}>
+                        {tab}
+                    </span>
+                ))}
+                <span className="movingBg" style={{left:left}}></span>
+            </div>
+            </div>
     )
 }
 
