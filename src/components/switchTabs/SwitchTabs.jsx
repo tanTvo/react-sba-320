d54222c42526e65db7ecfe33e948b060f5fcb0e8
Tan <tan10tvo@gmail.com>
2024-05-25 15:00:35 -0600

    carousel comp and import

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -5,14 +5,21 @@ const SwitchTabs = ({data, onTabChange}) =>{
     const [left, setLeft] = useState(0);
 
     const activeTab = (tab, index) => {
-        setLeft()
+        setLeft(index * 100)
+        setTimeout(() => {
+            setSelectedTab(index)
+        }, 300);
+        onTabChange(tab, index)
     } 
 
     return (
         <div className="switchingTabs">
             <div className="tabItems">
                 {data.map((tab, index) => (
-                    <span key={index} className={`tabItem`}>
+                    <span 
+                    key={index} 
+                    className={`tabItem ${selectedTab === index ? "active" : ""}`}
+                    onClick={()=> activeTab(tab, index)}>
                         {tab}
                     </span>
                 ))}
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/UseFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
-ContentWrapper
-SwitchTabs
 
 const Trending = () => {
+    const [endpoint, setEndpoint] = useState("day")
+    const{data, loading} = useFetch(`/trending/all/${endpoint}`);
 
     const onTabChange = (tab) => {
-
-    }
+        setEndpoint(tab === "Day" ? "day" : "week");
+     };
     return <div className="carouselSection">
         <ContentWrapper>
             <span className="carouselTitle">Trending</span>
