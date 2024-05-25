f4ad1f7db66a43d7126d1167e6181074a9a85826
Tan <tan10tvo@gmail.com>
2024-05-25 14:14:30 -0600

    import switchTab into trending

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 ContentWrapper
+SwitchTabs
 
 const Trending = () => {
     return <div className="carouselSection">
         <ContentWrapper>
             <span className="carouselTitle">Trending</span>
+            <SwitchTabs data={["Day", "Week"]}/>
         </ContentWrapper>
     </div>;
 
