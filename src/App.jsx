d16c52a673c94ce82922ba30cada958d2d96f233
Tan <tan10tvo@gmail.com>
2024-05-25 10:02:38 -0600

    hero banner and new hooks folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from 'react'
 import {fetchDataFromApi} from "./utils/api"
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
-import Details from './pages/details/Details';
-import SearchResult from './pages/searchResult/SearchResult';
-import Explore from './pages/explore/Explore';
-import PageNotFound from './pages/404/pageNotFound';
+ import Details from './pages/details/Details';
+ import SearchResult from './pages/searchResult/SearchResult';
+ import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
 
 function App() {
 
@@ -31,11 +32,19 @@ function App() {
 
 
 
-  return <div className='App'>App
-
-    {url?.total_pages}
-  
-  </div>;
+  return (
+    <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+  );
   
 }
 
