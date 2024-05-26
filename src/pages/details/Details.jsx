5d06fa8c5dc1d9084beaca9836ad0313525c6227
Tan <tan10tvo@gmail.com>
2024-05-25 22:23:13 -0600

    playbtn comp

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./style.scss"
 import useFetch from "../../hooks/UseFetch";
 import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 
 
 const Details = () => {
-    const {mediaType, id} = useParams();
-    const {data, loading} = useFetch(`/movie/${mediaType}/${id}`)
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
     return (
-        <div>Footer</div>
+        <div>
+            <DetailsBanner/>
+
+            </div>
     )
 }
 
