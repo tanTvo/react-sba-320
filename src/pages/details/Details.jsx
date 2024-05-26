3f25fcceb5e15b9793ee8f48b46785ec654a58b4
Tan <tan10tvo@gmail.com>
2024-05-25 22:00:06 -0600

    details comp

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./style.scss"
+import useFetch from "../../hooks/UseFetch";
+import { useParams } from "react-router-dom";
+
+
 
 const Details = () => {
+    const {mediaType, id} = useParams();
+    const {data, loading} = useFetch(`/movie/${mediaType}/${id}`)
+
     return (
         <div>Footer</div>
     )
