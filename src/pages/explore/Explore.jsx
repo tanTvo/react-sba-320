adc799eff882b7431cd7e30eef6cf26ea2213c18
Tan <tan10tvo@gmail.com>
2024-05-26 09:27:58 -0600

    explore

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -33,6 +33,69 @@ const Explore = () => {
     const [sortby, setSortby] = useState(null);
     const {mediaType} = useParams();
 
+    const { data: genresData } = useFetch(`/genre/${mediaType}/list`);
+
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/discover/${mediaType}`, filters).then((res) => {
+            setData(res);
+            setPageNum((prev) => prev + 1);
+            setLoading(false);
+        });
+    };
+
+    const fetchNextPageData = () => {
+        fetchDataFromApi(
+            `/discover/${mediaType}?page=${pageNum}`,
+            filters
+        ).then((res) => {
+            if (data?.results) {
+                setData({
+                    ...data,
+                    results: [...data?.results, ...res.results],
+                });
+            } else {
+                setData(res);
+            }
+            setPageNum((prev) => prev + 1);
+        });
+    };
+
+    useEffect(() => {
+        filters = {};
+        setData(null);
+        setPageNum(1);
+        setSortby(null);
+        setGenre(null);
+        fetchInitialData();
+    }, [mediaType]);
+
+    const onChange = (selectedItems, action) => {
+        if (action.name === "sortby") {
+            setSortby(selectedItems);
+            if (action.action !== "clear") {
+                filters.sort_by = selectedItems.value;
+            } else {
+                delete filters.sort_by;
+            }
+        }
+
+        if (action.name === "genres") {
+            setGenre(selectedItems);
+            if (action.action !== "clear") {
+                let genreId = selectedItems.map((g) => g.id);
+                genreId = JSON.stringify(genreId).slice(1, -1);
+                filters.with_genres = genreId;
+            } else {
+                delete filters.with_genres;
+            }
+        }
+
+        setPageNum(1);
+        fetchInitialData();
+    };
+
+    
     return (
         <div>Explore</div>
     )
