78b8f8ff368f0b6a05c66a2165d8fd836399374e
Tan <tan10tvo@gmail.com>
2024-05-26 09:02:28 -0600

    sort

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -10,6 +10,21 @@ import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
 
+let filters = {};
+
+const sortbyData = [
+    { value: "popularity.desc", label: "Popularity Descending" },
+    { value: "popularity.asc", label: "Popularity Ascending" },
+    { value: "vote_average.desc", label: "Rating Descending" },
+    { value: "vote_average.asc", label: "Rating Ascending" },
+    {
+        value: "primary_release_date.desc",
+        label: "Release Date Descending",
+    },
+    { value: "primary_release_date.asc", label: "Release Date Ascending" },
+    { value: "original_title.asc", label: "Title (A-Z)" },
+];
+
 const Explore = () => {
     return (
         <div>Explore</div>
