3aab149109d525e27ef68b0c3ed4db02d323928e
Tan <tan10tvo@gmail.com>
2024-05-25 18:56:47 -0600

    fix carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,9 +15,9 @@ import Genres from "../genres/Genres";
 
 import "./style.scss";
 
-const Carousel = ({data, loading}) => {
+const Carousel = ({ data, loading, endpoint, title }) => {
     const carouselContainer = useRef();
-    const {url} = useSelector((state) => state.home)
+    const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
 
     const navigation = (dir) => {
@@ -43,66 +43,76 @@ const Carousel = ({data, loading}) => {
                     <div className="date skeleton"></div>
                 </div>
             </div>
-        )
-    }
+        );
+    };
 
-    return <div className="carousel">
-       <ContentWrapper>
-        <BsFillArrowLeftCircleFill
-            className="carouselLeftNav arrow"
-            onClick={() => navigation("left")} 
-        />
-        <BsFillArrowRightCircleFill
-        className="carouselRightNav arrow"
-        onClick={() => navigation("right")} 
-        />
-             {!loading ? (
+    return (
+        <div className="carousel">
+            <ContentWrapper>
+                {title && <div className="carouselTitle">{title}</div>}
+                <BsFillArrowLeftCircleFill
+                    className="carouselLeftNav arrow"
+                    onClick={() => navigation("left")}
+                />
+                <BsFillArrowRightCircleFill
+                    className="carouselRighttNav arrow"
+                    onClick={() => navigation("right")}
+                />
+                {!loading ? (
                     <div className="carouselItems" ref={carouselContainer}>
                         {data?.map((item) => {
                             const posterUrl = item.poster_path
                                 ? url.poster + item.poster_path
                                 : PosterFallback;
-                            return(
-                            <div 
-                            key={item.id}
-                            className="carouselItems">
-                                
-                                <div className="posterBlock">
-                                    <Img src={posterUrl}/>
-                                    <CircleRating rating={item.vote_average.toFixed(1)}/>
-                                    <Genres
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="carouselItem"
+                                    onClick={() =>
+                                        navigate(
+                                            `/${item.media_type || endpoint}/${
+                                                item.id
+                                            }`
+                                        )
+                                    }
+                                >
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl} />
+                                        <CircleRating
+                                            rating={item.vote_average.toFixed(
+                                                1
+                                            )}
+                                        />
+                                        <Genres
                                             data={item.genre_ids.slice(0, 2)}
                                         />
-                                </div>
+                                    </div>
                                     <div className="textBlock">
                                         <span className="title">
-                                            {item.title} || {item.name}
+                                            {item.title || item.name}
                                         </span>
                                         <span className="date">
-                                            {dayjs(item.release_Date).format(
+                                            {dayjs(item.release_date || item.first_air_date).format(
                                                 "MMM D, YYYY"
                                             )}
                                         </span>
                                     </div>
-                            </div>
-                        )
-                    })}
-                </div>
-            ) : (
-               <div className="loadingSkeleton">
-
-                {skItem()}
-               {skItem()}
-               {skItem()}
-               {skItem()}
-               {skItem()}
-               </div>
-              
-            )}
-       </ContentWrapper>
-
-        
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="loadingSkeleton">
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                    </div>
+                )}
+            </ContentWrapper>
         </div>
+    );
 };
 
 export default Carousel;
